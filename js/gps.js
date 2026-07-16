58ed16efc311a219a6f860960395c4dd71e62b08
CheeseAndRice007 <waynedavison007@gmail.com>
2026-07-16 10:57:14 +1000

    Connected gps.js module

---
+++ js/gps.js
@@ -0,0 +1,10 @@
+// ========================================
+// Road Tracker Beta 1
+// GPS Module
+// ========================================
+
+console.log("GPS Module Loaded");
+
+function initialiseGPS() {
+    console.log("Initialising GPS...");
+}
